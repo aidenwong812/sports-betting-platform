b52a3e862c049e26d9935da5139632cbdd79be34
Aiden Wong <wongaiden812@gmail.com>
2024-04-19 07:03:43 +0800

    add spinner

---
+++ components/Spinner.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+const Spinner = () => {
+  return (
+    <div className="spinner-container m-auto">
+      <div className="spinner"></div>
+      <style jsx>{`
+        .spinner-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100%;
+          width: 30px;
+        }
+        .spinner {
+          border: 4px solid transparent; /* Light grey */
+          border-top: 4px solid rgb(83 250 251); /* Blue */
+          border-left: 4px solid rgb(83 250 251); /* Blue */
+          border-radius: 50%;
+          width: 30px;
+          height: 30px;
+          animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Spinner;
