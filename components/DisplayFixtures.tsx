3cb066bbcff2bbd0692a86c771c48d27d8b99f97
Aiden Wong <wongaiden812@gmail.com>
2024-04-22 03:31:07 +0800

    feat: odd live data.

--- components/DisplayFixtures.tsx
+++ components/DisplayFixtures.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import formatDate from "@/lib/formatDate"
 import { Fixture } from "@/lib/types/fixture"
+import DisplayOdds from "./DisplayOdds"
 
 const DisplayFixtures = ({ fixtures }: { fixtures: Fixture[] }) => {
   return (
@@ -9,7 +10,7 @@ const DisplayFixtures = ({ fixtures }: { fixtures: Fixture[] }) => {
         fixtures.map(({ fixture, league, teams }) => (
           <div className="table-inner" key={fixture.id}>
             <div className="table-head">
-              <Link href="details" className="left-compo">
+              <Link href={`/details/${fixture.id}`} className="left-compo">
                 <span>
                   <img src={league.logo} alt="icon" height={30} />
                 </span>
@@ -24,52 +25,28 @@ const DisplayFixtures = ({ fixtures }: { fixtures: Fixture[] }) => {
             <div className="table-body">
               <ul className="table-body-left">
                 <li>
-                  <Link href="#0">
+                  <div>
                     <span>{teams.home.name}</span>
                     <span className="icon">
                       <img src={teams.home.logo} alt="flag" height={40} />
                     </span>
-                  </Link>
+                  </div>
                 </li>
                 <li>
-                  <Link href="#0" className="vs">
+                  <div className="vs">
                     VS
-                  </Link>
+                  </div>
                 </li>
                 <li>
-                  <Link href="#0">
+                  <div>
                     <span className="icon">
                       <img src={teams.away.logo} alt="flag" height={40} />
                     </span>
                     <span>{teams.away.name}</span>
-                  </Link>
+                  </div>
                 </li>
               </ul>
-              <div className="table-body-right">
-                <Link href="#0" className="table-pointing-box">
-                  <span className="list">1</span>
-                  <span>7.58</span>
-                </Link>
-                <Link href="#0" className="table-pointing-box">
-                  <span className="list">X</span>
-                  <span>8.08</span>
-                </Link>
-                <Link href="#0" className="table-pointing-box">
-                  <span className="list">2</span>
-                  <span>2.98</span>
-                </Link>
-                <Link href="#0" className="table-pointing-box">
-                  <span className="list">O 2.5</span>
-                  <span>1.84</span>
-                </Link>
-                <Link href="#0" className="table-pointing-box">
-                  <span className="list">U 2.5</span>
-                  <span>5.7</span>
-                </Link>
-                <Link href="#0" className="table-pointing-box">
-                  <span className="last-digit">+37</span>
-                </Link>
-              </div>
+              <DisplayOdds fixtureId={fixture.id} />
             </div>
           </div>
         ))
